b6dba13609e694e3defebe542817b145cb2cab3c
unknown <theuves@gmail.com>
2017-07-25 12:58:36 -0400

    algumas alterações

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,51 @@
 "use strict";
 
 var gulp = require("gulp");
-var watch = require("gulp-watch");
-var less = require("gulp-less");
 var csso = require("gulp-csso");
+var htmlmin = require("gulp-htmlmin");
+var less = require("gulp-less");
+var uglify = require("gulp-uglify");
+var watch = require("gulp-watch");
+
+/***/
+
+gulp.task("html", function () {
+  gulp.src("./src/index.html")
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest("./dist"));
+});
+
+gulp.task("js", function () {
+  gulp.src("./src/js/**/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./dist/js"));
+});
+
+gulp.task("css", function () {
+  gulp.src("./src/css/**/*.css")
+    .pipe(csso())
+    .pipe(gulp.dest("./dist/css"));
+});
+
+gulp.task("minify", [
+  "css",
+  "html",
+  "js"
+]);
+
+/***/
 
 gulp.task("less", function () {
-  gulp.src("./less/style.less")
+  gulp.src("./src/less/style.less")
     .pipe(less())
-    .pipe(csso())
-    .pipe(gulp.dest("./css/"));
+    .pipe(gulp.dest("./src/css"));
 });
 
 gulp.task("less-watch", function () {
-  return watch("./less/**/*.less", function () {
-    gulp.src("./less/style.less")
+  return watch("./src/less/**/*.less", function () {
+    gulp.src("./src/less/style.less")
       .pipe(less())
-      .pipe(csso())
-      .pipe(gulp.dest("./css/"));
+      .pipe(gulp.dest("./src/css"));
   });
 });
+
