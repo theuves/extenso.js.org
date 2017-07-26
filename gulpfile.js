696f042002b16ae338485cd06c4dfcb9def13aa4
unknown <theuves@gmail.com>
2017-07-25 20:57:18 -0400

    adiciona mais comandos

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,55 @@
 "use strict";
 
 var gulp = require("gulp");
+
+// Gulp...
 var csso = require("gulp-csso");
 var htmlmin = require("gulp-htmlmin");
 var less = require("gulp-less");
 var uglify = require("gulp-uglify");
 var watch = require("gulp-watch");
 
-/***/
+// Minificar HTML, CSS e JavaScript.
+// =================================
 
+// HTML.
 gulp.task("html", function () {
-  gulp.src("./src/index.html")
+  return gulp.src("./src/index.html")
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest("./dist"));
 });
 
-gulp.task("js", function () {
-  gulp.src("./src/js/**/*.js")
-    .pipe(uglify())
-    .pipe(gulp.dest("./dist/js"));
-});
-
+// CSS.
 gulp.task("css", function () {
-  gulp.src("./src/css/**/*.css")
+  return gulp.src("./src/css/**/*.css")
     .pipe(csso())
     .pipe(gulp.dest("./dist/css"));
 });
 
+// JavaScript.
+gulp.task("js", function () {
+  return gulp.src("./src/js/**/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./dist/js"));
+});
+
+// Minifica.
 gulp.task("minify", [
   "css",
   "html",
   "js"
 ]);
 
-/***/
+// Compilar LESS em CSS.
+// =====================
 
 gulp.task("less", function () {
-  gulp.src("./src/less/style.less")
+  return gulp.src("./src/less/style.less")
     .pipe(less())
     .pipe(gulp.dest("./src/css"));
 });
 
-gulp.task("less-watch", function () {
+gulp.task("watch", function () {
   return watch("./src/less/**/*.less", function () {
     gulp.src("./src/less/style.less")
       .pipe(less())
@@ -49,3 +57,20 @@ gulp.task("less-watch", function () {
   });
 });
 
+// Copiar componentes Bower no "dist".
+// ===================================
+
+gulp.task("bower", function () {
+  return gulp.src([
+      "./src/bower_components/**/*"
+    ])
+    .pipe(gulp.dest("./dist/bower_components"));
+});
+
+// Construção.
+// ===========
+
+gulp.src("default", [
+  "bower",
+  "minify"
+]);
