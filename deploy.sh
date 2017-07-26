11570661d73e2f087f5ba57cd663bac6f4cedf73
unknown <theuves@gmail.com>
2017-07-25 22:25:01 -0400

    algumas alterações

--- deploy.sh
+++ deploy.sh
@@ -3,31 +3,21 @@
 # Remove o diretório "dist".
 rm -rf dist
 
-# Clona o repositório.
+# Clonar o repositório.
 git clone -b gh-pages https://github.com/theuves/extenso.js.org dist
 
-# Constrói os arquivos da página.
+# Construir os arquivos da página.
 npm run build
 
-# Entra no repositório.
+# Entrar no repositório.
 cd dist
 
-# Commit.
+# "Commitar".
 git add .
 git commit -m "atualiza"
 
-# Envia as alterações.
+# Enviar as alterações.
 git push origin gh-pages
 
-# Entra no branch gh-pages.
-git checkout gh-pages
-
-# Se estiver no branch gh-pages.
-if [[ $(git rev-parse --abbrev-ref HEAD) -eq "gh-pages" ]]; then
-
-  # Ignora alterações (caso tenha).
-  git checkout -- .
-
-  # Sincroniza.
-  git pull
-fi
+# Sincronizar com o gh-pages.
+git pull origin gh-pages
