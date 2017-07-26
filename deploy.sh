9e1e42341b873ef0824b2bf6f62779caf4fbe7f3
unknown <theuves@gmail.com>
2017-07-25 21:47:06 -0400

    adiciona deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Remove o diretório "dist".
+rm -rf dist
+
+# Clona o repositório.
+git clone -b gh-pages https://github.com/theuves/extenso.js.org dist
+
+# Constrói os arquivos da página.
+npm run build
+
+# Entra no repositório.
+cd dist
+
+# Commit.
+git add .
+git commit -m "atualiza"
+
+# Envia as alterações.
+git push origin gh-pages
+
+# Entra no branch gh-pages.
+git checkout gh-pages
+
+# Ignora alterações (caso tenha)
+git checkout -- .
+
+# Sincroniza.
+git pull
