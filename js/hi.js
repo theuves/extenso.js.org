ec59d0dc446151873d44e84fd3e2fbedeefb4bb7
Matheus Alves <theuves@gmail.com>
2019-12-31 19:10:39 -0400

    normaliza hi.js

--- js/hi.js
+++ js/hi.js
@@ -1,34 +1,42 @@
+/**
+ * Relaçar sintaxe de código JavaScript.
+ *
+ * Essa função não é complete sendo criada especialmente para o
+ * caso de uso do site <extenso.js.org> podendo haver
+ * várias falhas se usado para outros fins.
+ *
+ * @author Matheus Alves
+ * @license MIT
+ */
 function hi(code) {
-  
-  //
-  // Functions
-  //
+
+  // functions
   code = code.replace(
     /(\w+)\(/g,
     '<span class="hi-func">$1</span>('
   )
 
-  //
-  // Strings
-  //
+  // strings
   code = code.replace(
     /('.[^']*'|'')/g,
     '<span class="hi-str">$1</span>'
   )
 
-  //
-  // Object properties
-  //
+  // object properties
   code = code.replace(
     /\b(\w+)\b\s*:/g,
     '<span class="hi-obj-prop">$1</span>:'
   )
 
-  //
-  // Comments
-  //
+  // comments
   code = code.replace(/(\/\/.*)/g, function (_, value) {
+
+    // remove definições de outros tipos de dados
+    // anteriores para que elas não sobreponham a definição
+    // do cometário que deve ser prioridade
     value = value.replace(/<span[^>]*>(.*)<\/span>/g, '$1')
+
+    // define coment
     value = `<span class="hi-comment">${value}</span>`
     
     return value
